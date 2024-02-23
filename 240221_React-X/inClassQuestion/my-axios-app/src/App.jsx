c77bd461bcaaad8fe9982a17a24cddf36b2870e4
Aman <amanmittle1234@gmail.com>
2024-02-24 02:09:22 +0530

    empty File

--- 240221_React-X/inClassQuestion/my-axios-app/src/App.jsx
+++ 240221_React-X/inClassQuestion/my-axios-app/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
-import axios from 'axios';
+import axios from 'axios'
 import './App.css'
 
 function App() {
   const [data, setData] = useState([]);
+
+  const BASE_PRODUCTION_URL = import.meta.env.VITE_BASE_PRODUCTION;
+  const BASE_DEV_URL = import.meta.env.VITE_BASE_DEV;
+  const currentEnvironment = import.meta.env.MODE
 
   async function fetchDataAxios() {
     try {
@@ -17,7 +21,7 @@ function App() {
 
   async function postRequest() {
     try {
-      
+
       const config = {
         method: "post",
         url: "https://jsonplaceholder.typicode.com/posts",
@@ -37,7 +41,7 @@ function App() {
 
   async function putRequest() {
     try {
-      
+
       const config = {
         method: "put",
         url: "https://reqres.in/api/users/2",
@@ -56,7 +60,7 @@ function App() {
 
   async function patchRequest() {
     try {
-      
+
       const config = {
         method: "patch",
         url: "https://jsonplaceholder.typicode.com/posts/1",
@@ -74,7 +78,7 @@ function App() {
 
   async function deleteRequest() {
     try {
-      
+
       const config = {
         method: "delete",
         url: "https://jsonplaceholder.typicode.com/posts/1"
@@ -89,11 +93,11 @@ function App() {
 
   async function paramsDataAxios() {
     try {
-      
+
       const config = {
         url: "https://jsonplaceholder.typicode.com/posts",
-        params:{
-          page:2
+        params: {
+          page: 2
         }
       }
 
@@ -106,6 +110,8 @@ function App() {
 
   return (
     <>
+      <h1>Current Environment</h1>
+      <h1>{currentEnvironment}</h1>
       <button onClick={fetchDataAxios}>Get Post</button>
       <button onClick={paramsDataAxios}>Get Post customly</button>
       <button onClick={postRequest}>Make Post Req..</button>
