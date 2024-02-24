78bcce5a99ac18d69e50bda94820107b3bbcdc41
Aman <amanmittle1234@gmail.com>
2024-02-24 22:42:58 +0530

    Slight change in <Link/>

--- 240224_Evaluation/Evaluation24FEB-React_Tech_Product_List_Routing/src/Components/ProductCard.jsx
+++ 240224_Evaluation/Evaluation24FEB-React_Tech_Product_List_Routing/src/Components/ProductCard.jsx
@@ -1,12 +1,11 @@
-import { Link, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function ProductCard({ brand, category, price, id }) {
-    const {product_id} = useParams();
     return (
         <tr data-testid="item">
             <td>{id}</td>
             <td data-testid="brand">
-                <Link to={`/dashboard/${product_id}`}>{brand}</Link>
+                <Link to={`/dashboard/${id}`}>{brand}</Link>
                 {/* //     add Link here and keep the data-testid="brand" on Link tag       */}
             </td>
             <td data-testid="category">
