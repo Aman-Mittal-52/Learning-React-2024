433258e2c1c144a3f9a27ed7ad6a9c2aeb08d9c1
Aman <amanmittle1234@gmail.com>
2024-02-24 22:53:56 +0530

    Change in Alt img Text and title

--- 240224_Evaluation/Evaluation24FEB-React_Tech_Product_List_Routing/src/Routes/SingleProductPage.jsx
+++ 240224_Evaluation/Evaluation24FEB-React_Tech_Product_List_Routing/src/Routes/SingleProductPage.jsx
@@ -1,40 +1,54 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-function SingleProductPage(){
+function SingleProductPage() {
 
-    const {product_id} = useParams()
-    const [ data , setData]=  useState([])
-    async function  fetchSingleProduct(){
+    const { product_id } = useParams();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [data, setData] = useState([]);
+    async function fetchSingleProduct() {
+        setLoading(true)
         try {
-            let res= await fetch(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-tech-products/${product_id}`);
+            let res = await fetch(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-tech-products/${product_id}`);
             const data = await res.json();
             setData(data.data);
+            setLoading(false)
         } catch (error) {
+            setError(true)
+            setLoading(false)
             console.log(error);
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchSingleProduct()
     })
+
+    if (loading) {
+        return <Loader />
+    }
+
+    if (error) {
+        return <h1>ERROR FETCH</h1>
+    }
     return (
-        <div data-testid="products-container">   
+        <div data-testid="products-container">
             <div>
                 <h3 data-testid="product-brand">{data.brand}</h3>
             </div>
             <div >
-            <img data-testid="product-image" src={data.img}/>
+                <img data-testid="product-image" src={data.img} alt={data.brand} />
             </div>
             <div data-testid="product-category">{data.category}
             </div>
-           
+
             <div data-testid="product-details">{data.details}
             </div>
             <div data-testid="product-price">{Float32Array.price}
             </div>
 
-            </div>
+        </div>
     )
 }
 export default SingleProductPage
